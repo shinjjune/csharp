494c85b43597063ff0f9b955ebdd700533affa0f
신준혁 <@DESKTOP-1TF1DNV>
2020-01-02 15:07:55 +0900

    test2 commit

---
+++ csharp/Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Program2
+    {
+        static void Main2(string[] args)
+        {
+            while(true)
+            {
+                string inputString;
+
+                Console.Write("what is the input string?");
+                inputString = Console.ReadLine();
+                if(inputString == "")
+                {
+                    Console.WriteLine("Input any key");
+                }
+                else
+                {
+                    Console.WriteLine("{0} has {1} characters", inputString, inputString.Length);
+                }
+            }
+
+        }
+    }
+}
