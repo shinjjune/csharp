67c497e0c57a7b438eeb31d84a6ca0aefe23bc03
신준혁 <@DESKTOP-1TF1DNV>
2020-01-02 15:44:39 +0900

    Mad Libs

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -9,6 +9,7 @@ namespace csharp
     class Program
     {
         static void Main()
+            // Hello
         {
             Random rand = new Random();
 
@@ -21,6 +22,7 @@ namespace csharp
             Console.WriteLine(string.Format("{0}{1}{2}{3}", s1[rand.Next(0, s1.Length)], s, s2[rand.Next(0, s2.Length)], s3[rand.Next(0, s3.Length)]));
 
             Console.WriteLine("");
+            // Count
             // while (true)
             {
                 string inputString;
@@ -36,6 +38,36 @@ namespace csharp
                     Console.WriteLine("{0} has {1} characters", inputString, inputString.Length);
                 }
             }
+            // Double Quotation
+            {
+                string inputQuote, inputName;
+                Console.Write("what is the quote?");
+                inputQuote = Console.ReadLine();
+                Console.Write("who said it? ");
+                inputName = Console.ReadLine();
+                Console.WriteLine(inputName + " says, \"" + inputQuote + "\"");
+                Console.ReadLine();
+            }
+            // Mad Libs
+            {
+                // while(true)
+                {
+                    string snoun, sverb, sadjective, sadverb, sinterjection;
+                    Console.Write("Enter a noun :");
+                    snoun = Console.ReadLine();
+                    Console.Write("Enter a verb :");
+                    sverb = Console.ReadLine();
+                    Console.Write("Enter a adjective :");
+                    sadjective = Console.ReadLine();
+                    Console.Write("Enter a adverb :");
+                    sadverb = Console.ReadLine();
+                    Console.Write("Enter a interjection :");
+                    sinterjection = Console.ReadLine();
+
+                    Console.WriteLine("Do you {1} your {2} {0} {3}? That's {4}!", snoun, sverb, sadjective, sadverb, sinterjection);
+                }
+            }
+
         }
     }
    
